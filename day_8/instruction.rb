8b3017a3fa8aa7d6f429f8ffe91de202c7ddf6a6
hrakotobe <haga.rakotobe@hivency.com>
2020-12-08 22:35:22 +0100

    day 8

--- day_8/instruction.rb
+++ day_8/instruction.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
+
 require 'byebug'
 
 class Instruction
-  attr_accessor :execution_count, :operation, :parameter
+  attr_accessor :execution_count, :operation, :parameter, :number
 
-  def initialize(line)
+  def initialize(line, number = nil)
     @execution_count = 0
+    @number = number
     parse(line)
   end
 
@@ -13,6 +15,20 @@ class Instruction
     pair = line.split(' ')
     @operation = pair[0]
     @parameter = pair[1].to_i
+  end
+
+  def nop_or_jmp?
+    %w[nop jmp].include?(operation)
+  end
+
+  def invert_nop_or_jmp
+    return unless nop_or_jmp?
+
+    @operation = operation == 'nop' ? 'jmp' : 'nop'
+  end
+
+  def reset_counter
+    @execution_count = 0
   end
 
   def run(state)
--- day_8/run.rb
+++ day_8/run.rb
@@ -5,17 +5,42 @@ require './instruction'
 
 input = File.open('input.txt').readlines
 
-def run_instructions(input)
-  instructions = input.map { |line| Instruction.new(line) }
-  return if instructions.empty?
+def parse_instruction(input)
+  input.map.with_index { |line, index| Instruction.new(line, index + 1) }
+end
 
+def run_instructions(instructions)
   state = { accumulator: 0, pointer: 0 }
+  instructions.each(&:reset_counter)
 
-  while state[:pointer] < instructions.length && instructions[state[:pointer]].execution_count < 1
+  while state[:pointer] < instructions.length
+    if instructions[state[:pointer]].execution_count > 0
+      state[:death_loop] = true
+      state[:code] = instructions[state[:pointer]].operation
+      break
+    end
     instructions[state[:pointer]].run(state)
   end
   state
 end
 
-state = run_instructions(input)
-puts state[:accumulator]
+def fix_instructions(instructions)
+  candidates = instructions.select(&:nop_or_jmp?)
+  iterations_left = candidates.count
+
+  while iterations_left.positive?
+    iterations_left -= 1
+    candidate = candidates[iterations_left]
+    candidate.invert_nop_or_jmp
+
+    state = run_instructions(instructions)
+    return state if state[:death_loop].nil?
+
+    candidate.invert_nop_or_jmp # revert
+  end
+  nil
+end
+
+# state = run_instructions(parse_instruction(input))
+state = fix_instructions(parse_instruction(input))
+puts state.nil? ? 'Failed to fix' : state[:accumulator]
