7eb38e6db3c7a08f1fc990645a057a92c15beed9
hrakotobe <haga.rakotobe@hivency.com>
2020-12-13 23:24:25 +0100

    day 12 - part 2

--- day_12/run.rb
+++ day_12/run.rb
@@ -6,6 +6,9 @@ require './ship'
 input = File.open('input.txt')
 ship = Ship.new
 
-input.each { |line| ship.process(line.strip) }
+input.each do |line| 
+  ship.process(line.strip)
+  puts "Did: #{line.strip} => waypoint: E: #{ship.w_east} N: #{ship.w_north} O: #{ship.orientation} (ship: E:#{ship.east} N:#{ship.north})"
+end
 
 puts ship.manhattan_distance
--- day_12/ship.rb
+++ day_12/ship.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class Ship
-  attr_accessor :east, :north, :orientation
+  attr_accessor :east, :north, :orientation, :w_east, :w_north
   attr_reader :degree_reference, :orientation_reference
 
   def initialize
     @east = 0
     @north = 0
     @orientation = 'E'
+    @w_east = 10
+    @w_north = 1
 
-    @degree_reference = { 0 => 'E', 90 => 'S', 180 => 'W', 270 => 'N' }
+    @degree_reference = { 0 => 'E', 90 => 'N', 180 => 'W', 270 => 'S' }
     @orientation_reference = degree_reference.invert
   end
 
@@ -18,9 +20,9 @@ class Ship
     value = instruction[1..-1].to_i
 
     if %w[N E S W].include?(command)
-      move(command, value)
+      move_waypoint(command, value)
     elsif command == 'F'
-      move(orientation, value)
+      move(value)
     elsif %w[R L].include?(command)
       rotate(command, value)
     else
@@ -28,22 +30,37 @@ class Ship
     end
   end
 
-  def move(direction, value)
+  def move_waypoint(direction, value)
     case direction
     when 'N'
-      @north += value
+      @w_north += value
     when 'S'
-      @north -= value
+      @w_north -= value
     when 'E'
-      @east += value
+      @w_east += value
     when 'W'
-      @east -= value
+      @w_east -= value
     end
   end
 
+  def move(value)
+    @east += @w_east * value
+    @north += @w_north * value
+  end
+
   def rotate(direction, value)
-    target_value = orientation_reference[orientation] + (direction == 'R' ? value : -1 * value)
-    @orientation = degree_reference[target_value % 360]
+    rotation_angle = (direction == 'L' ? value : -1 * value)
+    target_value = orientation_reference[orientation] + rotation_angle
+    target_orientation = degree_reference[target_value % 360]
+    return if target_orientation == orientation
+
+    @orientation = target_orientation 
+    
+    rotation_radian = rotation_angle * Math::PI / 180 
+    current_east = w_east
+    current_north = w_north
+    @w_east = (current_east * Math.cos(rotation_radian) - current_north * Math.sin(rotation_radian)).round
+    @w_north = (current_east * Math.sin(rotation_radian) + current_north * Math.cos(rotation_radian)).round
   end
 
   def manhattan_distance
